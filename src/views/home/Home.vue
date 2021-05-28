7e5b741ee6ca5006519ff47c38103aee99a86b08
zhaoyuhui328 <248081983@qq.com>
2021-05-28 08:41:18 +0800

    first commit zyh

---
+++ src/views/home/Home.vue
@@ -0,0 +1,171 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles = "['流行', '新款', '精选']"
+                 class="tab-control"
+                 @tabClick = "tabClick"/>
+    <goods-list :goods = "showGoods"></goods-list>
+
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+      <li>列表31</li>
+      <li>列表32</li>
+      <li>列表33</li>
+      <li>列表34</li>
+      <li>列表35</li>
+      <li>列表36</li>
+      <li>列表37</li>
+      <li>列表38</li>
+      <li>列表39</li>
+      <li>列表40</li>
+      <li>列表41</li>
+      <li>列表42</li>
+      <li>列表43</li>
+      <li>列表44</li>
+      <li>列表45</li>
+      <li>列表46</li>
+      <li>列表47</li>
+      <li>列表48</li>
+      <li>列表49</li>
+      <li>列表50</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import NavBar from "../../components/common/navbar/NavBar";
+import TabControl from "../../components/content/tabControl/TabControl";
+import GoodsList from "../../components/content/goods/GoodsList";
+
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
+import {getHomeMultidata, getHomeGoods} from "../../network/home";
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    TabControl,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    GoodsList
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []},
+      },
+      currentType : 'pop'
+    }
+  },
+  created() {
+    // 1.请求多个数据
+    this.getHomeMultidata()
+    // 2.请求商品数据
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    // 网络请求
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        // console.log(res);
+        this.banners =res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      })
+    },
+
+    // 事件监听
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = 'pop'
+          break
+        case 1:
+          this.currentType = 'new'
+          break
+        case 2:
+          this.currentType = 'sell'
+          break
+      }
+    }
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #home {
+    padding-top: 44px;
+    /*height: 100vh;*/
+    /*position: relative;*/
+  }
+
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #ffffff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+  }
+</style>
---
+++ test.js
@@ -0,0 +1,29 @@
+// console.log('Hello world!')
+// Vuex  状态管理工具
+
+// 状态管理？
+/*
+*  1.用户登录状态 、 用户名称、头像、地理位置信息等等
+*  2.比如商品的收藏、购物车中的物品等等
+*
+*  这些信息，我们都可以统一放在一个地方，对他进行保存和管理，而且他们还是响应式的
+*/
+
+
+/*
+*  解构赋值
+*
+*  1.对象的解构
+* const obj = {
+*   name: 'kobe',
+*   age: 18
+* }
+* const {name1,age1} = obj
+*
+* 2. 数组的解构
+* const names = ['maizn','kobe','Sam']
+* const [name1, name2, name3] = names
+*/
+
+
+// 最新接口地址：baseURL = "http://152.136.185.210:7878/api/m5"
